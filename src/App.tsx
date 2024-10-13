ae82661e4518e9fba8cbc5802b48808107e934e9
Eissa Faheem <eissafaheem@gmail.com>
2024-10-13 13:00:43 +0530

    feat: input with error message

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React, { FormEventHandler } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { SubmitHandler, useForm } from "react-hook-form";
+import { EissaInputField } from './conponents/EissaInputField/EissaInputField';
+import { DevTool } from "@hookform/devtools";
+
+interface FormData {
+  firstName: string;
+  email: string;
+  age: number;
+}
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, touchedFields },
+    control
+  } = useForm<FormData>(
+    {
+      mode: "all",
+    }
+  );
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data); // You can handle your form data here
+  };
+
+  return (
+    <div className="App" style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <EissaInputField<FormData>
+          label="First Name"
+          name="firstName"
+          register={register}
+          error={errors?.firstName}
+          rules={{ required: "First name is required", maxLength:{message:"Not more than 5", value:5}, pattern:{value:/eissa/, message:"Should contain eissa"} }}
+          isTouched={touchedFields.firstName}
+        />
+        <button type="submit">Sub</button>
+      </form>
+      <DevTool control={control}/>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/conponents/EissaInputField/EissaInputField.tsx
@@ -0,0 +1,38 @@
+import styles from "./EissaInputField.module.css";
+import {
+    Path,
+    UseFormRegister,
+    FieldValues,
+    FieldError,
+    RegisterOptions,
+} from "react-hook-form";
+
+interface EissaInputFieldProps<T extends FieldValues> {
+    label: string;
+    name: Path<T>;
+    register: UseFormRegister<T>;
+    rules?: RegisterOptions<T>;
+    error?: FieldError;
+    isTouched?: boolean;
+    placeholder?: string;
+}
+
+export const EissaInputField = <T extends FieldValues>(
+    props: EissaInputFieldProps<T>
+) => {
+    const { label, name, register, rules, error,isTouched ,placeholder = "" } = props;
+    return (
+        <div className={styles.input_container}>
+            <div className={styles.input_wrapper}>
+                <input
+                    type="text"
+                    id={name}
+                    placeholder={placeholder}
+                    {...register(name, rules)}
+                />
+                <label htmlFor={name}>{label}</label>
+            </div>
+            <div className={`${styles.errorMessage} ${isTouched && error?.message ? styles.showErrorMessage: styles.hideErrorMessage}`}> {error?.message}</div>
+        </div>
+    );
+};
