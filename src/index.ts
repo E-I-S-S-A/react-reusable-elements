f20ca6e06af810d1ceb80485443f088bdbc2c9c0
Eissa Faheem <eissafaheem@gmail.com>
2024-10-31 22:10:03 +0530

    feat: export checkbox

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export { default as EissaButton } from "./components/EissaButton/EissaButton";
 export { default as EissaInputField } from "./components/EissaInputField/EissaInputField";
+export { default as EissaButton } from "./components/EissaButton/EissaButton";
 export { default as EissaLoader } from "./components/EissaLoader/EissaLoader";
+export { default as EissaCheckbox } from "./components/EissaCheckbox/EissaCheckbox";
