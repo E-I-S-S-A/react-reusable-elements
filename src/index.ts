9aebdfd7f11b030d37b0fd2ff382c90c47793b14
Eissa Faheem <eissafaheem@gmail.com>
2025-01-28 22:36:05 +0530

    feat: new version and eissa avatr

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as EissaButton } from "./components/EissaButton/EissaButton";
 export { default as EissaLoader } from "./components/EissaLoader/EissaLoader";
 export { default as EissaCheckbox } from "./components/EissaCheckbox/EissaCheckbox";
 export { default as EissaModal } from "./components/EissaModal/EissaModal";
+export { default as EissaAvatar } from "./components/EissaAvatar/EissaAvatar";
