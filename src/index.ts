a7f1a462844a361d97d9509a9f81f44368dee078
Eissa Faheem <eissafaheem@gmail.com>
2025-08-11 00:07:16 +0530

    Exported EissaTextLogo and published version

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { default as EissaLoader } from "./components/EissaLoader/EissaLoader";
 export { default as EissaCheckbox } from "./components/EissaCheckbox/EissaCheckbox";
 export { default as EissaModal } from "./components/EissaModal/EissaModal";
 export { default as EissaAvatar } from "./components/EissaAvatar/EissaAvatar";
+export { default as EissaTextLogo } from "./components/EissaTextLogo/EissaTextLogo";
